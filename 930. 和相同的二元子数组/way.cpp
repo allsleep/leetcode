fd19e05be33ab7a68fe93599edaa049d2dc50c68
linhan <linhan1208@outlook.com>
2021-08-16 14:36:08 +0800

    2

--- 930. 和相同的二元子数组/way.cpp
+++ 930. 和相同的二元子数组/way.cpp
@@ -1,8 +1,8 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-
-class Solution {
+class mySolution1 {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n = 0;
@@ -21,3 +21,22 @@ public:
         return n1 + n2;
     }
 };
+
+/*
+    
+*/
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int sum = 0;
+        unordered_map<int, int> cnt;
+        int ret = 0;
+        for (auto& num : nums) {
+            cnt[sum]++;
+            sum += num;
+            ret += cnt[sum - goal];
+        }
+        return ret;
+    }
+};
+
